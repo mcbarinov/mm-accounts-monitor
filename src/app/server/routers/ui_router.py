73382f869a50411475073aabfc747d3b3be51955
mcbarinov <mcbarinov@proton.me>
2025-04-08 14:17:12 +0300

    update

--- src/app/server/routers/ui_router.py
+++ src/app/server/routers/ui_router.py
@@ -1,3 +1,5 @@
+import logging
+import time
 from typing import Annotated
 
 import pydash
@@ -14,6 +16,9 @@ from app.server import utils
 from app.server.deps import View
 
 router = APIRouter(include_in_schema=False)
+
+
+logger = logging.getLogger(__name__)
 
 
 @cbv(router)
@@ -55,11 +60,16 @@ class CBV(View):
 
     @router.get("/groups")
     async def groups(self, network_type: Annotated[NetworkType | None, Query()] = None) -> HTMLResponse:
+        start = time.perf_counter()
         query = {"network_type": network_type} if network_type else {}
         groups = await self.core.db.group.find(query, "name")
+        logger.info("s1", extra={"elapsed": (time.perf_counter() - start) * 1000})
+
         coins = await self.core.coin_service.get_coins()
+        logger.info("s2", extra={"elapsed": (time.perf_counter() - start) * 1000})
         coins_by_network_type = await self.core.coin_service.get_coins_by_network_type()
-        return await self.render.html(
+        logger.info("s3", extra={"elapsed": (time.perf_counter() - start) * 1000})
+        res = await self.render.html(
             "groups.j2",
             groups=groups,
             coins=coins,
@@ -68,6 +78,8 @@ class CBV(View):
             coins_by_network_type=coins_by_network_type,
             form={"network_type": network_type},
         )
+        logger.info("s4", extra={"elapsed": (time.perf_counter() - start) * 1000})
+        return res
 
     @router.get("/groups/{group_id}")
     async def group(self, group_id: ObjectId) -> HTMLResponse:
