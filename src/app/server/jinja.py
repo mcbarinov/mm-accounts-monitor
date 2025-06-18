2776b0eb54a6146d844936a0eb680d9d69b95aaa
mcbarinov <mcbarinov@proton.me>
2025-06-18 17:50:04 +0300

    deps

--- src/app/server/jinja.py
+++ src/app/server/jinja.py
@@ -1,23 +1,10 @@
-from markupsafe import Markup
 from mm_base6 import JinjaConfig
 from mm_web3 import Network, NetworkType
 
 from app.core.types import AppCore
 
 
-async def header_info(_core: AppCore) -> Markup:
-    info = ""
-    return Markup(info)  # noqa: S704 # nosec
-
-
-async def footer_info(_core: AppCore) -> Markup:
-    info = ""
-    return Markup(info)  # noqa: S704 # nosec
-
-
-jinja_config = JinjaConfig(
-    header_info=header_info,
-    header_info_new_line=False,
-    footer_info=footer_info,
-    globals={"networks": list(Network), "network_types": list(NetworkType)},
-)
+class AppJinjaConfig(JinjaConfig[AppCore]):
+    filters = {}
+    globals = {"networks": list(Network), "network_types": list(NetworkType)}
+    header_info_new_line = False
